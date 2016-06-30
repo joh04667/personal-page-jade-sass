579f67e1f6d04d761d9717e638688cff037884d3
Kyle Johnson <joh04667@gmail.com>
2016-06-30 18:55:56 -0500

    moves scroll notify animation to container div. fades on scroll

--- client/client.js
+++ client/client.js
@@ -1,1 +1,47 @@
-var app = angular.module('MyApp', []); 
+var app = angular.module('MyApp', ['ngAnimate']);
+
+
+app.service('ListenerService', ['$window', '$rootScope', function($window, $rootScope) {
+
+    // function scrollDown(callback) {
+    //   $window.addEventListener('scroll', function(event) {
+    //     $rootScope.$broadcast('DOM.scroll', event);
+    //   });
+    // }
+    //
+    // return {
+    //   scrollDown: scrollDown
+    // };
+
+}]);
+
+
+app.controller('scroll', ['ListenerService', '$scope', '$window', '$timeout', function(ListenerService, $scope, $window, $timeout) {
+  $scope.scrolled = false;
+  $scope.scrollTick = false;
+
+  var scrollCount = 0;
+
+  $window.addEventListener('scroll',function() {
+    $scope.$broadcast('scroll', event);
+  });
+
+  $timeout(function() {
+    $scope.scrollTick = true;
+  }, 6000);
+
+  // var listen = $scope.$on('scroll', function() {
+  //   console.log('ddddd');
+  //   scrollCount ++;
+  //   if(scrollCount > 5) {
+  //     listen();
+  //     $scope.scrolled = true;
+  //     $scope.$digest();
+  //   }
+  // });
+
+  $scope.$on('scroll', function() {
+    $('.scroll-notify').css({'opacity':( 100-$(window).scrollTop()/2 )/100});
+  });
+
+}]);

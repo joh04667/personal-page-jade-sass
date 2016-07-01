1a533a25dd641b5b0c4d69651f050274ee80c018
Kyle Johnson <joh04667@gmail.com>
2016-06-30 22:17:37 -0500

    adds masonry object. waiting on photoshop to not crash to edit thumbnails

--- client/client.js
+++ client/client.js
@@ -1,46 +1,31 @@
-var app = angular.module('MyApp', ['ngAnimate']);
+var app = angular.module('MyApp', ['ngAnimate', 'wu.masonry']);
 
 
 app.service('ListenerService', ['$window', '$rootScope', function($window, $rootScope) {
 
-    // function scrollDown(callback) {
-    //   $window.addEventListener('scroll', function(event) {
-    //     $rootScope.$broadcast('DOM.scroll', event);
-    //   });
-    // }
-    //
-    // return {
-    //   scrollDown: scrollDown
-    // };
+    function scrollDown(callback) {
+      $window.addEventListener('scroll', function(event) {
+        $rootScope.$broadcast('DOM.scroll', event);
+      });
+    }
+
+    return {
+      scrollDown: scrollDown
+    };
 
 }]);
 
 
-app.controller('scroll', ['ListenerService', '$scope', '$window', '$timeout', function(ListenerService, $scope, $window, $timeout) {
-  $scope.scrolled = false;
+app.controller('scroll', ['ListenerService', '$scope', '$rootScope', '$timeout', function(ListenerService, $scope, $rootScope, $timeout) {
   $scope.scrollTick = false;
 
-  var scrollCount = 0;
-
-  $window.addEventListener('scroll',function() {
-    $scope.$broadcast('scroll', event);
-  });
+  ListenerService.scrollDown();
 
   $timeout(function() {
     $scope.scrollTick = true;
   }, 6000);
 
-  // var listen = $scope.$on('scroll', function() {
-  //   console.log('ddddd');
-  //   scrollCount ++;
-  //   if(scrollCount > 5) {
-  //     listen();
-  //     $scope.scrolled = true;
-  //     $scope.$digest();
-  //   }
-  // });
-
-  $scope.$on('scroll', function() {
+  $rootScope.$on('DOM.scroll', function() {
     $('.scroll-notify').css({'opacity':( 100-$(window).scrollTop()/2 )/100});
   });
 

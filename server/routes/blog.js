4e430267da6acefd1983ffb1998c227f94fa40f6
Kyle Johnson <joh04667@gmail.com>
2016-07-27 18:46:29 -0500

    adds linking to blog posts based on what exists

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -6,25 +6,36 @@ var connectionString = require('../db/connection.js').connectionString;
 
 
 router.get('/', function(request, res) {
-  res.render('blog.jade', {title: 'Kyle Johnson\'s Blog'});
+  res.render('blog.jade', {title: 'Kyle Johnson\'s Blog', range: [2,3,4,5]});
 });
 
 router.get('/:article', function(request, res) {
   pg.connect(connectionString, function(err, client, done) {
     var result = [];
+    var range = [];
     var query = client.query(`SELECT * FROM posts WHERE id = ${request.params.article}`);
     query.on('row', row => {result.push(row)});
     query.on('err', err => {throw(err)});
     query.on('end', function() {
-      done();
+
+      var low = request.params.article - 2;
+      low = low < 1 ? 1 : low;
+      var high = low + 5;
+      var query = client.query(`SELECT id FROM posts WHERE id BETWEEN ${low} AND ${high}`);
+
+      query.on('row', row => {range.push(row.id)});
+      query.on('end', function() {
+
       result = result.length ? result[0] : false;
       if(!result) {
         res.sendStatus(404);
       } else {
       res.render('blog.jade', {title: 'Kyle Johnson\'s Blog #' + request.params.article,
                                header: result.title,
-                               postBody: result.body});
+                               postBody: result.body,
+                               range: range});
       }
+    });
     });
   });
 

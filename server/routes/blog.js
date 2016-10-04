0714858d5d5992efb376ddc469be719f7921fbab
joh04667 <joh04667@gmail.com>
2016-10-04 17:13:30 -0500

    further changes DB queries to use new utility functions yay

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -4,6 +4,7 @@ var jade = require('jade');
 var pg = require('pg');
 var connectionString = require('../db/connection.js').connectionString;
 var util = require('../../modules/util');
+var db = require('../../modules/Utilities/db');
 
 
 router.get('/', function(request, res) {
@@ -14,49 +15,37 @@ router.get('/', function(request, res) {
 
 // GET request for specific blog post
 router.get('/:article', function(request, res) {
-  // get post info from DB to template
-  pg.connect(connectionString, function(err, client, done) {
-    var result = [];
-    var range = [];
-    var query = client.query(`SELECT * FROM posts WHERE id = ${request.params.article}`);
-    query.on('row', row => {result.push(row)});
-    query.on('err', err => {throw(err)});
-    query.on('end', function() {
 
-      // logic to link to other posts
-      var low = request.params.article - 2;
-      low = low < 1 ? 1 : low;
-      var high = low + 4;
-      var query = client.query(`SELECT id FROM posts WHERE id BETWEEN ${low} AND ${high}`);
+  db.select(`SELECT * FROM posts WHERE id = ${request.params.article}`, function(result) {
+    // logic to link to other posts
+    var low = request.params.article - 2;
+    low = low < 1 ? 1 : low;
+    var high = low + 4;
 
-      query.on('row', row => {range.push(row.id)});
-      query.on('end', function() {
+    db.select(`SELECT id FROM posts WHERE id BETWEEN ${low} AND ${high}`, function(pageRange) {
 
-        client.end();
+      // get just first result for post id. if no result, set false. Sort it numerically
+      result = result.length ? result[0] : false;
 
-        // get just first result for post id. if no result, set false. Sort it numerically
-        result = result.length ? result[0] : false;
-        range = range.sort((a,b) => a - b);
-        // if no result found, 404 them.
-        if(!result) {
-          res.sendStatus(404);
-        } else {
+      // my stupid function returns rows of object so map it then sort it
+      var range = pageRange.map(s => s.id);
+      range.sort((a,b) => a - b);
 
-          res.render('blog.jade', {title: `Kyle Johnson\'s Blog #` + request.params.article,
-                                  article_id: request.params.article,
-                                  header: result.title,
-                                  postBody: result.body,
-                                  postDate: result.date_added,
-                                  range: range,
-                                  util: util,
-                                  current: request.params.article * 1});
-        }
-    });
+      // if no result found, 404 them.
+      if(!result) {
+        res.sendStatus(404);
+      } else {
+        res.render('blog.jade', {title: `Kyle Johnson\'s Blog #` + request.params.article,
+                                header: result.title,
+                                postBody: result.body,
+                                postDate: result.date_added,
+                                range: range,
+                                util: util,
+                                article_id: request.params.article * 1});
+      }
     });
   });
-
-
-
 });
+
 
 module.exports = router;

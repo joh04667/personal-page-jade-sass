05968f75ce8cd58ae429a8926f8be8e722c3803b
joh04667 <joh04667@gmail.com>
2016-10-04 19:47:18 -0500

    adds vmodal to edit posts on admin page. I will probably need to change the render of the post list from from Jade to front end

--- client/admin.js
+++ client/admin.js
@@ -1,46 +1,58 @@
-var app = angular.module('MyAdmin', ['angularTrix', 'ngAria', 'ngAnimate', 'ngMaterial']);
+var app = angular.module('MyAdmin', ['angularTrix', 'ngAria', 'ngAnimate', 'ngMaterial', 'vModal']);
 
 app.factory('DataService', ['$http', function($http) {
 
-  var result = {};
-  var GetPosts = function() {
-    $http.get('/admin/all').then(function(response) {
-      result.data = response.data;
-    });
-  };
+    var result = {};
+    var GetPosts = function() {
+        $http.get('/admin/all').then(function(response) {
+            result.data = response.data;
+        });
+    };
 
-  GetPosts();
+    GetPosts();
 
-  return {
-    result: result,
-    GetPosts: GetPosts
-  };
+    return {
+        result: result,
+        GetPosts: GetPosts
+    };
+
+}]);
+
+app.factory('MyModal', ['vModal', function(vModal) {
+    return vModal({
+        controller: 'ModalController',
+        templateUrl: 'views/edit_modal.html'
+    })
+}]);
+
+app.controller('ModalController', ['MyModal', 'DataService', '$scope', function(MyModal, DataService, $scope) {
+    $scope.close = MyModal.deactivate;
 
 }]);
 
 
-app.controller('PostListController', ['DataService', '$scope', function(DataService, $scope) {
-
-  $scope.edit = function(stuff) {
-    console.log(stuff);
-  };
+app.controller('PostListController', ['DataService', '$scope', 'MyModal', function(DataService, $scope, MyModal) {
+    $scope.edit = MyModal.activate;
 
 }]);
 
 app.controller('NewPostController', ['DataService', '$scope', '$http', function(DataService, $scope, $http) {
-  function resetScope() {
-    $scope.title = '';
-    $scope.body = '';
-  }
-
-  $scope.submit = function() {
-
-    if($scope.title && $scope.body) {
-      $http.post('/admin/new', {title: $scope.title, body: $scope.body}).then(function(response){
-        DataService.GetPosts();
-      });
-      resetScope();
+    function resetScope() {
+        $scope.title = '';
+        $scope.body = '';
     }
-  };
+
+    $scope.submit = function() {
+
+        if ($scope.title && $scope.body) {
+            $http.post('/admin/new', {
+                title: $scope.title,
+                body: $scope.body
+            }).then(function(response) {
+                DataService.GetPosts();
+            });
+            resetScope();
+        }
+    };
 
 }]);
